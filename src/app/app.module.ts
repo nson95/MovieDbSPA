f4b11a9809e61d8507c45c653bf1ccfc556b5c14
nson95 <nicksonnenberg95@gmail.com>
2019-04-03 16:37:59 -0400

    created all actor and movie components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { MovieListComponent } from './movie/movie-list/movie-list.component';
+import { MovieCreateComponent } from './movie/movie-create/movie-create.component';
+import { MovieEditComponent } from './movie/movie-edit/movie-edit.component';
+import { MovieDetailComponent } from './movie/movie-detail/movie-detail.component';
+import { ActorCreateComponent } from './actor/actor-create/actor-create.component';
+import { ActorListComponent } from './actor/actor-list/actor-list.component';
+import { ActorDetailComponent } from './actor/actor-detail/actor-detail.component';
+import { ActorEditComponent } from './actor/actor-edit/actor-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MovieListComponent,
+    MovieCreateComponent,
+    MovieEditComponent,
+    MovieDetailComponent,
+    ActorCreateComponent,
+    ActorListComponent,
+    ActorDetailComponent,
+    ActorEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
