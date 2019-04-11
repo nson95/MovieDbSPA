120693cc7216c0635a82e882142f37c3ff974e2c
nson95 <nicksonnenberg95@gmail.com>
2019-04-11 12:17:22 -0400

    movie list functional

--- src/app/movie/movie-list/movie-list.component.ts
+++ src/app/movie/movie-list/movie-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MovieService } from '../movie.service';
+import { Movie } from '../movie';
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
+  movies : Movie [];
 
-  constructor() { }
+  constructor(
+    private mvsvc: MovieService
+  ) { 
+
+  }
 
   ngOnInit() {
+    this.mvsvc.list()
+    .subscribe(resp => {
+      console.log("Movies: ", resp);
+      this.movies = resp.data;
+    });
   }
 
 }
